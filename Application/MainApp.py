65f93b301848a99b588a956b6b6baa1bc4deef73
is_ckRIP <ma.danilovv2001@gmail.com>
2022-12-08 20:28:02 +0300

    Добавлены элементы в поле "Настройки графиков"

--- Application/MainApp.py
+++ Application/MainApp.py
@@ -41,17 +41,16 @@ class MainWindow(QDialog):
         self._params_in_group_box_ = QGroupBox("Зондирующий сигнал")
         self._params_in_group_box_.setMaximumWidth(200)
 
-
+        # Задаём виджеты полей ввода и их размер
         self.input_frequency_in = QLineEdit()
         self.input_phase_in = QLineEdit()
         self.input_amplitude_in = QLineEdit()
-
         self.input_frequency_in.setFixedWidth(50)
         self.input_phase_in.setFixedWidth(50)
         self.input_amplitude_in.setFixedWidth(50)
 
+        # Создаем сетку для элементов
         _params_in_ = QGridLayout()
-
         _params_in_.addWidget(QLabel("Частота (Гц):"), 0, 0)
         _params_in_.addWidget(QLabel("Фаза:"), 1, 0)
         _params_in_.addWidget(QLabel("Амплитуда (мВ):"), 2, 0)
@@ -60,23 +59,22 @@ class MainWindow(QDialog):
         _params_in_.addWidget(self.input_amplitude_in, 2, 1)
         _params_in_.setColumnStretch(2, 1)
 
-
         self._params_in_group_box_.setLayout(_params_in_)
 
     def create_params_out(self):
         self._params_out_group_box_ = QGroupBox("Принимаемый сигнал")
         self._params_out_group_box_.setMaximumWidth(200)
 
+        # Задаём виджеты полей ввода и их размер
         self.input_frequency_out = QLineEdit()
         self.input_phase_out = QLineEdit()
         self.input_amplitude_out = QLineEdit()
-
         self.input_frequency_out.setFixedWidth(50)
         self.input_phase_out.setFixedWidth(50)
         self.input_amplitude_out.setFixedWidth(50)
 
+        # Создаем сетку для элементов
         _params_out_ = QGridLayout()
-
         _params_out_.addWidget(QLabel("Частота (Гц):"), 0, 0)
         _params_out_.addWidget(QLabel("Фаза:"), 1, 0)
         _params_out_.addWidget(QLabel("Амплитуда (мВ):"), 2, 0)
@@ -84,7 +82,6 @@ class MainWindow(QDialog):
         _params_out_.addWidget(self.input_phase_out, 1, 1)
         _params_out_.addWidget(self.input_amplitude_out, 2, 1)
         _params_out_.setColumnStretch(2, 1)
-
 
         self._params_out_group_box_.setLayout(_params_out_)
 
@@ -99,5 +96,18 @@ class MainWindow(QDialog):
         self._math_group_box_ = QGroupBox("Настройки графиков")
 
         math = QGridLayout()
+
+        self.button_generate = QPushButton("Отрисовать")
+        self.input_duration = QLineEdit()
+        self.input_time = QLineEdit()
+        self.input_time.setFixedWidth(50)
+        self.input_duration.setFixedWidth(50)
+
+        math.addWidget(QLabel("Частота дискретизации:"), 0, 0)
+        math.addWidget(QLabel("Время:"), 1, 0)
+        math.addWidget(self.input_duration, 0, 1)
+        math.addWidget(self.input_time, 1, 1)
+        math.addWidget(self.button_generate, 2, 2, 1, 2)
+        math.setColumnStretch(2, 1)
 
         self._math_group_box_.setLayout(math)
---
+++ Test.py
@@ -0,0 +1,9 @@
+from math import pi, cos
+
+coord = {}
+time = 10
+duration = 100
+
+for i in range(time*duration):
+    coord[i*1/duration] =
+print(coord)
